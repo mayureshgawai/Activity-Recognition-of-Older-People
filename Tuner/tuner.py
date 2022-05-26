3be0414430edfcac18121fc4b566f381ee80e345
mayuresh.gawai4799@gmail.com <mayuresh.gawai4799@gmail.com>
2022-05-26 14:01:22 +0530

    Commented some other part due to some issue. Main flow working fine and models are being saved successfully

---
+++ Tuner/tuner.py
@@ -0,0 +1,165 @@
+import pandas as pd
+from xgboost import XGBClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import roc_auc_score, accuracy_score
+import numpy as np
+
+class Model_Finder:
+    def __init__(self, file_object, logger):
+        self.file_object = file_object
+        self.logger = logger
+
+    def get_best_params_xgboost(self, X_train, y_train):
+
+        try:
+            params = {
+                'learning_rate': [0.5, 0.1, 0.01, 0.001],
+                'max_depth': [3, 5, 10, 20],
+                'n_estimators': [10, 50, 100, 200]
+            }
+
+            grid = GridSearchCV(XGBClassifier(objective='binary:logistic'), params, cv=5)
+            grid.fit(X_train, y_train)
+
+            learning_rate = grid.best_params_['learning_rate']
+            max_depth = grid.best_params_['max_depth']
+            n_estimators = grid.best_params_['n_estimators']
+
+            xgb = XGBClassifier(learning_rate=learning_rate, max_depth=max_depth, n_estimators=n_estimators)
+            un = y_train.unique()
+            xgb.fit(X_train, y_train)
+            return xgb
+        except Exception as e:
+            self.logger.log(self.file_object, "get_best_params_xgboost:: Error occcured while finding best params for training data for xgboot, "+str(e))
+
+
+    # def find_ccpAlpha_for_dt(self, dt_model, X_train, y_train):
+    #     path = dt_model.cost_complexity_pruning_path(X_train, y_train)
+    #     ccp_alpha = path['ccp_alphas']
+    #
+    #     dt_models = []
+    #     for ccp in ccp_alpha:
+    #         dt = DecisionTreeClassifier(ccp_alpha=ccp, random_state=10)
+    #         dt_models.append(dt)
+    #
+    #
+
+
+
+    def get_best_params_dt(self, X_train, y_train):
+        try:
+            params = {
+                'criterion': ['gini', 'entropy'],
+                'splitter': ['best', 'random'],
+                'max_depth': range(2, 40, 1),
+                'min_samples_split': range(2, 10, 1),
+                'min_samples_leaf': range(1, 10, 1)
+            }
+
+            grid = GridSearchCV(DecisionTreeClassifier() , param_grid=params, cv=5, )
+            # grid.fit(X_train, y_train)
+
+            # criterion = grid.best_params_['criterion']
+            # splitter = grid.best_params_['splitter']
+            # max_depth = grid.best_params_['max_depth']
+            # min_samples_split = grid.best_params_['min_samples_split']
+            # min_samples_leaf = grid.best_params_['min_samples_leaf']
+
+            criterion = 'entropy'
+            splitter = 'best'
+            max_depth = 33
+            min_samples_split = 2
+            min_samples_leaf = 1
+
+            dtree = DecisionTreeClassifier(criterion=criterion, splitter=splitter, max_depth=max_depth,
+                                           min_samples_leaf=min_samples_leaf, min_samples_split=min_samples_split)
+            dtree.fit(X_train, y_train)
+            return dtree
+        except Exception as e:
+            self.logger.log(self.file_object, "get_best_params_dt:: Error occcured while finding best params for training data for DecisionTree, "+str(e))
+
+    def get_best_params_svc(self, X_train, y_train):
+        try:
+            param = {
+                'kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
+                'C': [.1, .4, .6, 1, 2, 3, 100, 200, 500],
+                'gamma': [.001, .1, .4, .004, .003]
+            }
+
+            grid = GridSearchCV(SVC(), param_grid=param, cv=5 )
+            # grid.fit(X_train, y_train)
+
+            # kernel = grid.best_params_['kernel']
+            # c = grid.best_params_['C']
+            # gamma = grid.best_params_['gamma']
+            kernel = 'poly'
+            gamma = 0.001
+            c = 0.1
+            svc = SVC(kernel=kernel, C=c, gamma=gamma, probability=True)
+            svc.fit(X_train, y_train)
+
+            return svc
+        except Exception as e:
+            self.logger.log(self.file_object,
+                            "get_best_params_svc:: Error occcured while finding best params for training data for SVC, " + str(
+                                e))
+
+
+
+    def get_best_model(self, X_train, X_test, y_train, y_test):
+
+        try:
+            # xgb = self.get_best_params_xgboost(X_train, y_train)
+            # y_pred_xgb = xgb.predict(X_test)
+            #
+            # # if there is only one label in y, then roc_auc_score returns error. We will use accuracy in that case
+            # if(len(y_test.unique()) == 1):
+            #     xgboost_score = accuracy_score(y_test, y_pred_xgb)
+            #     self.logger.log(self.file_object, "get_best_model:: Accuracy Score: "+ str(xgboost_score))
+            # else:
+            #     xgboost_score =roc_auc_score(y_test, y_pred_xgb)
+            #     self.logger.log(self.file_object, "get_best_model:: AUC Score: "+str(xgboost_score))
+
+            un = y_test.unique()
+            dt = self.get_best_params_dt(X_train, y_train)
+            y_pred_dt = dt.predict_proba(X_test)
+            # y_pred_dt = pd.Series(y_pred_dt)
+
+            if (len(y_test.unique()) == 1):
+                dt_score = accuracy_score(y_test, y_pred_dt)
+                self.logger.log(self.file_object, "get_best_model:: Accuracy Score: " + str(dt_score))
+            else:
+                dt_score = roc_auc_score(y_test, y_pred_dt, multi_class='ovr')
+                self.logger.log(self.file_object, "get_best_model:: AUC Score: " + str(dt_score))
+
+            # svc = self.get_best_params_svc(X_train, y_train)
+            # # y_pred_svc = svc.predict(X_test)
+            # # y_pred_svc = pd.Series(y_pred_svc)
+            # y_pred_svc = svc.predict_proba(X_test)
+            #
+            # if (len(y_test.unique()) == 1):
+            #     svc_score = accuracy_score(y_test, y_pred_svc)
+            #     self.logger.log(self.file_object, "get_best_model:: Accuracy Score: " + str(svc_score))
+            # else:
+            #     svc_score = roc_auc_score(y_test, y_pred_svc, multi_class='ovr')
+            #     self.logger.log(self.file_object, "get_best_model:: AUC Score: " + str(svc_score))
+
+            # if(dt_score > svc_score):
+            return 'DecisionTree', dt
+            # else:
+            #     return 'SVC', svc
+
+
+            # if((xgboost_score > dt_score) and (xgboost_score > svc_score)):
+            #     return 'XGBoost', xgb
+            # elif((dt_score > xgboost_score) and (dt_score > svc_score)):
+            #     return 'DecisionTree', dt
+            # else:
+            #     return 'SVC', svc
+
+        except Exception as e:
+            self.logger.log(self.file_object, "get_best_model::Error Occurred in process of getting best model, "+str(e))
+
--- trainingModel.py
+++ trainingModel.py
@@ -10,6 +10,8 @@ from data_processing.processor import Processor
 from dbOperation.dbOperation import DBOperation
 from data_processing.clustering import Clustering
 from sklearn.model_selection import train_test_split
+from Tuner.tuner import Model_Finder
+from file_operations.file_opr import File_Operations
 
 class TrainValModel:
     def __init__(self, path):
@@ -20,6 +22,7 @@ class TrainValModel:
         self.processor = Processor()
         self.dbOperation = DBOperation()
         self.clustering = Clustering()
+        self.fileOpr = File_Operations()
 
 
     def train_val_data(self):
@@ -27,55 +30,67 @@ class TrainValModel:
             training datatset to the training phase'''
 
         try:
-            self.logger.log(self.file_object, "train_val_model()::Entered into Validation method")
-            # print("Entered into training method")
-            # Getting schema details
-            LengthOfDateStampInFile, LengthOfTimeStampInFile, NumberOfColumns, prefix, colNames = self.valid_data.validation_attributes()
-            # Get the regex
-            regex = self.valid_data.fileVal_regex()
-            # Check the file name for datestamp, timestamp and prefix
-            self.valid_data.check_file_name(regex, LengthOfDateStampInFile, LengthOfTimeStampInFile, prefix)
-            # Check column numbers, name and datatype
-            self.valid_data.column_length_validation()
-            self.logger.log(self.file_object, "train_val_model():column_length_validation()::Validation of Column length, name and dtype is done!")
-            # If there are columns paresent in dataset with no values, remove it
-            self.valid_data.validate_missing_values_in_columnn()
-            self.logger.log(self.file_object,"train_val_model():validate_missing_values_in_columnn()::Missing whole column values validation performed!")
-            self.logger.log(self.file_object, "Data Validation is done")
-
-            # Checking if any null values are there in columns. In case, then files will be moved to the NullRemovalFiles directory
-            self.processor.isNullPresent()
-            self.processor.impute_missing_values()
-            self.logger.log(self.file_object, "train_val_data()::Data Processing Ends here.")
-
-            self.logger.log(self.file_object, "train_val_data()::DB operation starts.")
-
-            dbname = self.parsed_yaml['dbconnect']['database_name']
-            tblname = self.parsed_yaml['dbconnect']['table_name']
-            trainPath = self.parsed_yaml['path']['validData']
-
-            # Create database
-            self.dbOperation.create_database(dbname)
-            # Create table
-            self.dbOperation.create_table(dbname, tblname)
-            # It will take files from valid data directory and insert into db.
-            self.dbOperation.insertIntoDB()
-            # It will select values from table and create final input file
-            self.dbOperation.select_and_create_input(tblname)
-            self.logger.log(self.file_object, "train_val_data()::DB operations Completed.")
+            # self.logger.log(self.file_object, "train_val_model()::Entered into Validation method")
+            # # print("Entered into training method")
+            # # Getting schema details
+            # LengthOfDateStampInFile, LengthOfTimeStampInFile, NumberOfColumns, prefix, colNames = self.valid_data.validation_attributes()
+            # # Get the regex
+            # regex = self.valid_data.fileVal_regex()
+            # # Check the file name for datestamp, timestamp and prefix
+            # self.valid_data.check_file_name(regex, LengthOfDateStampInFile, LengthOfTimeStampInFile, prefix)
+            # # Check column numbers, name and datatype
+            # self.valid_data.column_length_validation()
+            # self.logger.log(self.file_object, "train_val_model():column_length_validation()::Validation of Column length, name and dtype is done!")
+            # # If there are columns paresent in dataset with no values, remove it
+            # self.valid_data.validate_missing_values_in_columnn()
+            # self.logger.log(self.file_object,"train_val_model():validate_missing_values_in_columnn()::Missing whole column values validation performed!")
+            # self.logger.log(self.file_object, "Data Validation is done")
+            #
+            # # Checking if any null values are there in columns. In case, then files will be moved to the NullRemovalFiles directory
+            # self.processor.isNullPresent()
+            # self.processor.impute_missing_values()
+            # self.logger.log(self.file_object, "train_val_data()::Data Processing Ends here.")
+            #
+            # self.logger.log(self.file_object, "train_val_data()::DB operation starts.")
+            #
+            # dbname = self.parsed_yaml['dbconnect']['database_name']
+            # tblname = self.parsed_yaml['dbconnect']['table_name']
+            # trainPath = self.parsed_yaml['path']['validData']
+            #
+            # # Create database
+            # self.dbOperation.create_database(dbname)
+            # # Create table
+            # self.dbOperation.create_table(dbname, tblname)
+            # # It will take files from valid data directory and insert into db.
+            # self.dbOperation.insertIntoDB()
+            # # It will select values from table and create final input file
+            # self.dbOperation.select_and_create_input(tblname)
+            # self.logger.log(self.file_object, "train_val_data()::DB operations Completed.")
 
             input = self.parsed_yaml['path']['inputs']
 
             # self.clustering.elbow_plot()
             cluster_data = self.clustering.clusterDataset()
             file = os.listdir(input)
-            data = pd.read_csv(input+"/"+file[0])
+            data1 = pd.read_csv(input+"/"+file[0])
+            data = data1.drop(columns=['Unnamed: 0'])
             data['cluster'] = cluster_data
 
+            list_of_cluster = data['cluster'].unique()
 
+            for num  in list_of_cluster:
+                seperate_data = data[data['cluster'] == num]
+                seperate_features = seperate_data.drop(columns=['Label', 'cluster'])
+                label = seperate_data['Label']
 
+                # split the dataset into training and testing
+                X_train, X_test, y_train, y_test = train_test_split(seperate_features, label, test_size=0.25, random_state=30)
 
+                model_finder = Model_Finder(self.file_object, self.logger)
+
+                best_model_name, best_model = model_finder.get_best_model(X_train, X_test, y_train, y_test)
+                self.fileOpr.save_model(best_model_name+str(num), best_model)
 
 
         except Exception as e:
-            print("Unknown Error occured ", e)
+            print("Unknown Error occured ",+ str(e))
